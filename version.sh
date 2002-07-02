f74cf9a91cb9b9ab9a5c295e34a5b5d8f720e5fb
stephent <stephent@localhost>
2002-07-02 15:28:17 +0000

    [xemacs-hg @ 2002-07-02 15:28:17 by stephent] undo script brain-damage

--- version.sh
+++ version.sh
@@ -2,9 +2,8 @@
 emacs_is_beta=t
 emacs_major_version=21
 emacs_minor_version=5
-emacs_beta_version=7
-emacs_kit_version=bok choi
-xemacs_codename="broccoflower"
+emacs_beta_version=6
+xemacs_codename="bok choi"
 emacs_kit_version=
 infodock_major_version=4
 infodock_minor_version=0
