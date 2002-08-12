7c3653f4a3ac63962b66140fb151aec98fb51f56
james <james@localhost>
2002-08-12 14:45:34 +0000

    [xemacs-hg @ 2002-08-12 14:45:28 by james] Turn ORDINARY_LINK on for Linux.

--- src/s/linux.h
+++ src/s/linux.h
@@ -131,11 +131,11 @@ Boston, MA 02111-1307, USA.  */
 #ifdef LINUX_ELF
 #define UNEXEC "unexelf.o"
 #define UNEXEC_USE_MAP_PRIVATE
-#if 0
+
 /* mrb - Ordinary link is simple and effective */
 /* slb - Not any more ... :-( */
+/* jwj - slb's comment does not seem relevant to current Linuxes */
 #define ORDINARY_LINK
-#endif /* 0 */
 
 /* I still think ORDINARY_LINK should be the default, but since slb
    insists, ORDINARY_LINK will stay on until we expunge the dump code.
