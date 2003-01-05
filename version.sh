82a9fbaab98c97f44cc60de201263fc4587ce704
youngs <youngs@localhost>
2003-01-05 05:13:45 +0000

    [xemacs-hg @ 2003-01-05 05:13:26 by youngs] XEmacs 21.5.10 "burdock" is released.

--- version.sh
+++ version.sh
@@ -2,9 +2,9 @@
 emacs_is_beta=t
 emacs_major_version=21
 emacs_minor_version=5
-emacs_beta_version=9
-xemacs_codename="brussels sprouts"
-xemacs_extra_name="(+CVS-20030104)"
+emacs_beta_version=10
+xemacs_codename="burdock"
+xemacs_extra_name=
 emacs_kit_version=
 infodock_major_version=4
 infodock_minor_version=0
