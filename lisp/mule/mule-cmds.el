e038b0e4fc0db341a37277b351aab14120b46c19
stephent <stephent@localhost>
2003-02-10 17:47:44 +0000

    [xemacs-hg @ 2003-02-10 17:47:38 by stephent] use lang environment to set charset list <87u1fcf2ru.fsf@tleepslib.sk.tsukuba.ac.jp>

--- lisp/mule/mule-cmds.el
+++ lisp/mule/mule-cmds.el
@@ -756,7 +756,8 @@ the language environment for the major languages of Western Europe."
 
   ;; Fit the charsets preferences in unicode conversions for the
   ;; language environment.
-  (set-language-unicode-precedence-list (charset-list))
+  (set-language-unicode-precedence-list
+   (get-language-info language-name 'charset))
 
   (run-hooks 'set-language-environment-hook)
   (force-mode-line-update t))
