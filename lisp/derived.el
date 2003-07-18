79406a22a5696818563699f2344f086f22286587
stephent <stephent@localhost>
2003-07-18 07:42:48 +0000

    [xemacs-hg @ 2003-07-18 07:42:47 by stephent] fix derived-mode-merge-syntax-tables <8765m0jnb1.fsf@tleepslib.sk.tsukuba.ac.jp>

--- lisp/derived.el
+++ lisp/derived.el
@@ -342,12 +342,20 @@ Where the new table already has an entry, nothing is copied from the old one."
       ;; check for inheritance.
       (map-char-table
        #'(lambda (key value)
-	   (if (eq ?@ (char-syntax-from-code value))
-	       (map-char-table #'(lambda (key1 value1)
-				   (put-char-table key1 value1 new))
-			       old
-			       key)))
-       new)
+	   (let ((newval (get-range-char-table key new 'multi)))
+	     (cond ((eq newval 'multi)	; OK, dive into the class hierarchy
+		    (map-char-table
+		     #'(lambda (key1 value1)
+			 (when (eq ?@ (char-syntax-from-code
+				       (get-range-char-table key new ?@)))
+			   (put-char-table key1 value new))
+			 nil)
+		     new
+		     key))
+		   ((eq ?@ (char-syntax-from-code newval)) ;; class at once
+		    (put-char-table key value new))))
+	   nil)
+       old)
     ;; pre-20.0
     (let ((idx 0)
 	  (end (min (length new) (length old))))
