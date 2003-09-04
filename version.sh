f4eee78a1d73a44c77022c20e15295df7b6f292e
youngs <youngs@localhost>
2003-09-04 00:05:17 +0000

    [xemacs-hg @ 2003-09-04 00:05:04 by youngs] XEmacs 21.5.15 "celery" is released.

--- version.sh
+++ version.sh
@@ -2,9 +2,9 @@
 emacs_is_beta=t
 emacs_major_version=21
 emacs_minor_version=5
-emacs_beta_version=14
-xemacs_codename="cassava"
-xemacs_extra_name="(+CVS-20030903)"
+emacs_beta_version=15
+xemacs_codename="celery"
+xemacs_extra_name=
 emacs_kit_version=
 infodock_major_version=4
 infodock_minor_version=0
