cedc5c2f7b78b58fa6b327d9cf9de81bd5201390
ben <ben@localhost>
2005-02-03 23:59:16 +0000

    [xemacs-hg @ 2005-02-03 23:59:15 by ben] document that paragraphs needs easy-mmode
    dumped-lisp.el: Document that paragraphs needs easy-mmode.

--- lisp/dumped-lisp.el
+++ lisp/dumped-lisp.el
@@ -106,7 +106,7 @@ in dumped-lisp.el and is not itself listed.")
        "iso8859-1"		; This must be before any modes
 				; (sets standard syntax table.)
        "easy-mmode"		; Added for 21.5.
-       "paragraphs"
+       "paragraphs"             ; needs easy-mmode
        "easymenu"		; Added for 20.3.
        "lisp-mode"
        "text-mode"
