100160bc1d449e9bb387be0146a34d99f7a32b3c
crestani <crestani@localhost>
2005-06-01 10:51:36 +0000

    [xemacs-hg @ 2005-06-01 10:51:34 by crestani] * ralloc.c: * ralloc.c (r_alloc_free): Call refill_memory_reserve only if MC_ALLOC is not defined.

--- src/ralloc.c
+++ src/ralloc.c
@@ -61,7 +61,9 @@ typedef unsigned char *POINTER;
 #include "getpagesize.h"
 
 #include <string.h>
+#ifndef MC_ALLOC
 void refill_memory_reserve (void);
+#endif /* not MC_ALLOC */
 
 #else	/* Not emacs.  */
 
@@ -984,7 +986,9 @@ r_alloc_free (POINTER *ptr)
   *ptr = 0;
 
 #ifdef emacs
+#ifndef MC_ALLOC
   refill_memory_reserve ();
+#endif /* not MC_ALLOC */
 #endif
 }
 
