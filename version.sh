3eb67523a0f057e28ec8faa50f076f6928a48b84
viteno <viteno@localhost>
2005-08-19 21:51:12 +0000

    [xemacs-hg @ 2005-08-19 21:51:12 by viteno] Update xemacs_extra_name.

--- version.sh
+++ version.sh
@@ -8,5 +8,5 @@ emacs_kit_version=
 infodock_major_version=4
 infodock_minor_version=0
 infodock_build_version=8
-xemacs_extra_name="(+CVS-20050818)"
+xemacs_extra_name="(+CVS-20050819)"
 xemacs_release_date="2005-02-18"
