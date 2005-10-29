94eb67579ae1fe959ede6f6d3e39aedf6329ac4f
youngs <youngs@localhost>
2005-10-29 07:18:21 +0000

    [xemacs-hg @ 2005-10-29 07:18:19 by youngs] 2005-10-29  Steve Youngs  <steve@sxemacs.org>
    	* unexelf.c (unexec): Fix data types.
    	From: Andrey Slusar <anrays@gmail.com>

--- src/unexelf.c
+++ src/unexelf.c
@@ -565,8 +565,8 @@ round_up (ElfW(Addr) x, ElfW(Addr) y)
  *
  */
 int
-unexec (Extbyte *new_name, Extbyte *old_name, unsigned int UNUSED (data_start),
-	unsigned int UNUSED (bss_start), unsigned int UNUSED (entry_address))
+unexec (Extbyte *new_name, Extbyte *old_name, uintptr_t UNUSED (data_start),
+	uintptr_t UNUSED (bss_start), uintptr_t UNUSED (entry_address))
 {
   int new_file, old_file, new_file_size;
 
