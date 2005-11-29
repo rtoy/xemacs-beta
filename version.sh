d38bc634fc0de3ca6635411d412114c9b03e4e3b
stephent <stephent@localhost>
2005-11-29 17:04:24 +0000

    [xemacs-hg @ 2005-11-29 17:04:05 by stephent] Undo inadvertant release commits.

--- version.sh
+++ version.sh
@@ -2,11 +2,11 @@
 emacs_is_beta=t
 emacs_major_version=21
 emacs_minor_version=5
-emacs_beta_version=24
-xemacs_codename="dandelion"
+emacs_beta_version=23
+xemacs_codename="daikon"
 emacs_kit_version=
 infodock_major_version=4
 infodock_minor_version=0
 infodock_build_version=8
-xemacs_extra_name=""
-xemacs_release_date="2005-11-29"
+xemacs_extra_name="(+CVS-20051128)"
+xemacs_release_date="2005-10-26"
