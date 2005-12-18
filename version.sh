e49aa23c11b0d198382e590281c991ec01256f35
stephent <stephent@localhost>
2005-12-18 19:07:19 +0000

    [xemacs-hg @ 2005-12-18 19:04:51 by stephent] XEmacs 21.5.24 "dandelion" is released.

--- version.sh
+++ version.sh
@@ -2,11 +2,11 @@
 emacs_is_beta=t
 emacs_major_version=21
 emacs_minor_version=5
-emacs_beta_version=23
-xemacs_codename="daikon"
+emacs_beta_version=24
+xemacs_codename="dandelion"
 emacs_kit_version=
 infodock_major_version=4
 infodock_minor_version=0
 infodock_build_version=8
-xemacs_extra_name="(+CVS-20051217)"
-xemacs_release_date="2005-10-26"
+xemacs_extra_name=""
+xemacs_release_date="2005-12-19"
