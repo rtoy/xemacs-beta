b10af485340b98ff7e94e3a91b4b983ac7a5fc1b
crestani <crestani@localhost>
2006-01-20 17:59:50 +0000

    [xemacs-hg @ 2006-01-20 17:59:48 by crestani] * dynarr.c (Dynarr_realloc): Determine size of memory region to copy correctly, fix types. * dynarr.c (Dynarr_lisp_realloc): Determine size of memory region to copy correctly. * dynarr.c (Dynarr_resize): Call Dynarr_realloc with number of elements instead of memory region size.

--- src/dynarr.c
+++ src/dynarr.c
@@ -129,16 +129,17 @@ Use the following global variable:
 static int Dynarr_min_size = 8;
 
 static void
-Dynarr_realloc (Dynarr *dy, Bytecount new_size)
+Dynarr_realloc (Dynarr *dy, int new_size)
 {
   if (DUMPEDP (dy->base))
     {
       void *new_base = malloc (new_size);
-      memcpy (new_base, dy->base, dy->max > new_size ? dy->max : new_size);
+      memcpy (new_base, dy->base, 
+	      (dy->max < new_size ? dy->max : new_size) * dy->elsize);
       dy->base = new_base;
     }
   else
-    dy->base = xrealloc (dy->base, new_size);
+    dy->base = xrealloc (dy->base, new_size * dy->elsize);
 }
 
 void *
@@ -158,13 +159,13 @@ DEFINE_LRECORD_IMPLEMENTATION ("dynarr", dynarr,
 			       Dynarr);
 
 static void
-Dynarr_lisp_realloc (Dynarr *dy, Elemcount new_size)
+Dynarr_lisp_realloc (Dynarr *dy, int new_size)
 {
   void *new_base = alloc_lrecord_array (dy->elsize, new_size, dy->lisp_imp);
   void *old_base = dy->base;
   if (dy->base)
     memcpy (new_base, dy->base, 
-	    (dy->max > new_size ? dy->max : new_size) * dy->elsize);
+	    (dy->max < new_size ? dy->max : new_size) * dy->elsize);
   dy->base = new_base;
   if (old_base)
     mc_free (old_base);
@@ -205,9 +206,9 @@ Dynarr_resize (void *d, Elemcount size)
       if (dy->lisp_imp)
 	Dynarr_lisp_realloc (dy, newsize);
       else
-	Dynarr_realloc (dy, newsize*dy->elsize);
+	Dynarr_realloc (dy, newsize);
 #else /* not NEW_GC */
-      Dynarr_realloc (dy, newsize*dy->elsize);
+      Dynarr_realloc (dy, newsize);
 #endif /* not NEW_GC */
       dy->max = newsize;
     }
