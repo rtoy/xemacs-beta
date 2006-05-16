591009d2201e75bacc8a83a2db3b13d534ac2e5c
stephent <stephent@localhost>
2006-05-16 08:25:02 +0000

    [xemacs-hg @ 2006-05-16 08:22:21 by stephent] XEmacs 21.5.27 "fiddleheads" is released.

--- version.sh
+++ version.sh
@@ -2,11 +2,11 @@
 emacs_is_beta=t
 emacs_major_version=21
 emacs_minor_version=5
-emacs_beta_version=26
-xemacs_codename="endive"
+emacs_beta_version=27
+xemacs_codename="fiddleheads"
 emacs_kit_version=
 infodock_major_version=4
 infodock_minor_version=0
 infodock_build_version=8
-xemacs_extra_name="(+CVS-20060515)"
-xemacs_release_date="2006-04-01"
+xemacs_extra_name=""
+xemacs_release_date="2006-05-16"
