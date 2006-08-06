83c8335d32e14749b10ea6a42bb98b92ac37b97a
aidan <aidan@localhost>
2006-08-06 16:37:33 +0000

    [xemacs-hg @ 2006-08-06 16:37:31 by aidan] Add an informative assertion and comment to snarf-documentation.

--- src/doc.c
+++ src/doc.c
@@ -742,6 +742,10 @@ when doc strings are referred to in the dumped Emacs.
       if (p != end)
 	{
 	  end = qxestrchr (p, '\n');
+	  /* If you trigger a failure with of this assertion, you probably
+	     configured with --quick-build and need to to rebuild your DOC
+	     file. */
+	  assert((end - p - 2) > -1);
 	  sym = oblookup (Vobarray, p + 2, end - p - 2);
 	  if (SYMBOLP (sym))
 	    {
