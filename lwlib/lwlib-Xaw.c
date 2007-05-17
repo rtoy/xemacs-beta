2275cea2b4d077333123f99050518a46d090f956
stephent <stephent@localhost>
2007-05-17 15:06:04 +0000

    [xemacs-hg @ 2007-05-17 15:06:04 by stephent] Fix crash with wm delete of dialog. <87sl9v8ot1.fsf@uwakimon.sk.tsukuba.ac.jp>

--- lwlib/lwlib-Xaw.c
+++ lwlib/lwlib-Xaw.c
@@ -599,11 +599,14 @@ wm_delete_window (Widget shell, XtPointer UNUSED (closure),
     abort ();
   XtSetArg (al [0], XtNchildren, &kids);
   XtGetValues (shell, al, 1);
-  if (!kids || !*kids)
-    abort ();
-  widget = kids [0];
-  if (! XtIsSubclass (widget, dialogWidgetClass))
-    abort ();
+  if (!kids || !*kids) abort ();
+
+  for (widget = *kids;
+       widget && ! XtIsSubclass (widget, dialogWidgetClass);
+       widget = *++kids)
+    ;
+  if (!widget) abort ();
+
   id = lw_get_widget_id (widget);
   if (! id) abort ();
 
