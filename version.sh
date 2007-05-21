489ade450f8198d55b48af6213c21cec05c5e0e4
stephent <stephent@localhost>
2007-05-21 05:02:43 +0000

    [xemacs-hg @ 2007-05-21 04:59:52 by stephent] XEmacs 21.5.28 "fuki" is released.

--- version.sh
+++ version.sh
@@ -2,11 +2,11 @@
 emacs_is_beta=t
 emacs_major_version=21
 emacs_minor_version=5
-emacs_beta_version=27
-xemacs_codename="fiddleheads"
+emacs_beta_version=28
+xemacs_codename="fuki"
 emacs_kit_version=
 infodock_major_version=4
 infodock_minor_version=0
 infodock_build_version=8
-xemacs_extra_name="(+CVS-20070520)"
-xemacs_release_date="2006-05-16"
+xemacs_extra_name=""
+xemacs_release_date="2007-05-21"
