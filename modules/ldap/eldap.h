de28d7c47d222b388042b865527454b1731a0e6a
stephent <stephent@localhost>
2007-06-22 16:58:38 +0000

    [xemacs-hg @ 2007-06-22 16:58:36 by stephent] Declare use of deprecated LDAP interface.

--- modules/ldap/eldap.h
+++ modules/ldap/eldap.h
@@ -22,6 +22,13 @@ Boston, MA 02111-1307, USA.  */
 #define INCLUDED_eldap_h_
 
 #include <lber.h>
+/* #### NEEDS REWRITE!
+   Thanks to Mats Lidell <matsl@xemacs.org> for the report & patch:
+   <871wgnqunm.fsf@spencer.lidell.homelinux.net>
+   "See http://www.openldap.org/faq/data/cache/1278.html.
+   Temporary workaround would be use the deprecated interface. Long term
+   solution is a rewrite." */
+#define LDAP_DEPRECATED 1
 #include <ldap.h>
 
 /*
