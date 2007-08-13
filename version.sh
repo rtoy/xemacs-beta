4e3a38baf0c84fce5561413aeeb831028ebd9ef1
cvs <cvs@localhost>
2007-08-13 10:54:53 +0200

    Import from CVS: tag r21-1-5

--- version.sh
+++ version.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 emacs_major_version=21
 emacs_minor_version=1
-emacs_beta_version=4
-xemacs_codename="Arches"
+emacs_beta_version=5
+xemacs_codename="Badlands"
 infodock_major_version=4
 infodock_minor_version=0
 infodock_build_version=7
