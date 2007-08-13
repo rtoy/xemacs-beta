88cacf65194aa156e5e3fef5b8511cbcf11fba59
cvs <cvs@localhost>
2007-08-13 10:54:28 +0200

    Import from CVS: tag r21-1-4

--- version.sh
+++ version.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 emacs_major_version=21
 emacs_minor_version=1
-emacs_beta_version=3
-xemacs_codename="Acadia"
+emacs_beta_version=4
+xemacs_codename="Arches"
 infodock_major_version=4
 infodock_minor_version=0
 infodock_build_version=7
