2f4f6db0ee67102bfae1d8249f5ff96a564f30c6
Aidan Kehoe <kehoea@parhasard.net>
2008-08-05 08:37:17 +0200

    Revamp the Arabic support. Create greek-iso-8bit-with-esc, arabic-iso-8bit-with-esc.
    src/ChangeLog addition:
    
    2008-08-05  Aidan Kehoe  <kehoea@parhasard.net>
    
    	* mule-charset.c (complex_vars_of_mule_charset):
    	Remove Vcharset_arabic_iso8859_7.
    	* lisp.h: Remove Vcharset_arabic_iso8859_7.
    	See commentary in lisp/mule/iso-with-esc.el for motivation.
    
    lisp/ChangeLog addition:
    
    2008-08-05  Aidan Kehoe  <kehoea@parhasard.net>
    
    	* mule/iso-with-esc.el (greek-iso-8bit-with-esc):
    	(arabic-iso-8bit-with-esc):
    	Add these two here. Move the implementation of the
    	'arabic-iso8859-6  character set here, with commentary on why that
    	is reasonable.
    	* mule/arabic.el (iso-8859-6):
    	Add iso-8859-6, windows-1256 implementations using
    	make-8-bit-coding-system. Remove our non-standard Mule character
    	sets.
    	* unicode.el (load-unicode-tables): Remove Arabic since it's no
    	longer dumped.
    	* mule/mule-msw-init-late.el: Remove Arabic.
    	* mule/mule-category.el (predefined-category-list): Remove
    	Arabic.
    
    etc/ChangeLog addition:
    
    2008-08-05  Aidan Kehoe  <kehoea@parhasard.net>
    
    	* HELLO: Encode the Arabic using UTF-8 sequences, not ISO-8859-6.

--- lisp/mule/iso-with-esc.el
+++ lisp/mule/iso-with-esc.el
@@ -98,3 +98,108 @@
   charset-g3 t
    no-iso6429 t
    mnemonic "MIME/Hbrw"))
+
+;;;###autoload
+(make-coding-system
+ 'greek-iso-8bit-with-esc 'iso2022 "MIME ISO-8859-7"
+ '(charset-g0 ascii
+   charset-g1 greek-iso8859-7
+   charset-g2 t
+   charset-g3 t
+   mnemonic "Grk"))
+
+;; ISO 8859-6 is such a useless character set that it seems a waste of
+;; codespace to dump it. Let me count the ways: 
+;; 
+;; 1. It doesn't support Persian or Urdu, let alone Sinhalese, despite
+;;    plenty of unallocated code points.
+;;
+;; 2. It doesn't encode all the vowel diacritics (the Harakaat) despite that
+;;    they are necessary, even for the Arabs, for basic things like
+;;    dictionary entries, children's books, and occasional disambiguation.
+;;
+;; 3. The Arabs don't use it, they use Windows-1256, which also supports
+;;    Persian, at least, as well as the French characters necessary in
+;;    Lebanon and North Africa.
+
+(make-charset
+ 'arabic-iso8859-6 
+ "Right-Hand Part of Latin/Arabic Alphabet (ISO/IEC 8859-6): ISO-IR-127"
+ '(dimension 1
+   registry "ISO8859-6"
+   chars 96
+   columns 1
+   direction r2l
+   final ?G
+   graphic 1
+   short-name "RHP of ISO8859/6"
+   long-name "RHP of Arabic (ISO 8859-6): ISO-IR-127"))
+
+(loop
+  for (iso8859-6 unicode)
+  in '((#xA0 #x00A0) ;; NO-BREAK SPACE
+       (#xA4 #x00A4) ;; CURRENCY SIGN
+       (#xAC #x060C) ;; ARABIC COMMA
+       (#xAD #x00AD) ;; SOFT HYPHEN
+       (#xBB #x061B) ;; ARABIC SEMICOLON
+       (#xBF #x061F) ;; ARABIC QUESTION MARK
+       (#xC1 #x0621) ;; ARABIC LETTER HAMZA
+       (#xC2 #x0622) ;; ARABIC LETTER ALEF WITH MADDA ABOVE
+       (#xC3 #x0623) ;; ARABIC LETTER ALEF WITH HAMZA ABOVE
+       (#xC4 #x0624) ;; ARABIC LETTER WAW WITH HAMZA ABOVE
+       (#xC5 #x0625) ;; ARABIC LETTER ALEF WITH HAMZA BELOW
+       (#xC6 #x0626) ;; ARABIC LETTER YEH WITH HAMZA ABOVE
+       (#xC7 #x0627) ;; ARABIC LETTER ALEF
+       (#xC8 #x0628) ;; ARABIC LETTER BEH
+       (#xC9 #x0629) ;; ARABIC LETTER TEH MARBUTA
+       (#xCA #x062A) ;; ARABIC LETTER TEH
+       (#xCB #x062B) ;; ARABIC LETTER THEH
+       (#xCC #x062C) ;; ARABIC LETTER JEEM
+       (#xCD #x062D) ;; ARABIC LETTER HAH
+       (#xCE #x062E) ;; ARABIC LETTER KHAH
+       (#xCF #x062F) ;; ARABIC LETTER DAL
+       (#xD0 #x0630) ;; ARABIC LETTER THAL
+       (#xD1 #x0631) ;; ARABIC LETTER REH
+       (#xD2 #x0632) ;; ARABIC LETTER ZAIN
+       (#xD3 #x0633) ;; ARABIC LETTER SEEN
+       (#xD4 #x0634) ;; ARABIC LETTER SHEEN
+       (#xD5 #x0635) ;; ARABIC LETTER SAD
+       (#xD6 #x0636) ;; ARABIC LETTER DAD
+       (#xD7 #x0637) ;; ARABIC LETTER TAH
+       (#xD8 #x0638) ;; ARABIC LETTER ZAH
+       (#xD9 #x0639) ;; ARABIC LETTER AIN
+       (#xDA #x063A) ;; ARABIC LETTER GHAIN
+       (#xE0 #x0640) ;; ARABIC TATWEEL
+       (#xE1 #x0641) ;; ARABIC LETTER FEH
+       (#xE2 #x0642) ;; ARABIC LETTER QAF
+       (#xE3 #x0643) ;; ARABIC LETTER KAF
+       (#xE4 #x0644) ;; ARABIC LETTER LAM
+       (#xE5 #x0645) ;; ARABIC LETTER MEEM
+       (#xE6 #x0646) ;; ARABIC LETTER NOON
+       (#xE7 #x0647) ;; ARABIC LETTER HEH
+       (#xE8 #x0648) ;; ARABIC LETTER WAW
+       (#xE9 #x0649) ;; ARABIC LETTER ALEF MAKSURA
+       (#xEA #x064A) ;; ARABIC LETTER YEH
+       (#xEB #x064B) ;; ARABIC FATHATAN
+       (#xEC #x064C) ;; ARABIC DAMMATAN
+       (#xED #x064D) ;; ARABIC KASRATAN
+       (#xEE #x064E) ;; ARABIC FATHA
+       (#xEF #x064F) ;; ARABIC DAMMA
+       (#xF0 #x0650) ;; ARABIC KASRA
+       (#xF1 #x0651) ;; ARABIC SHADDA
+       (#xF2 #x0652));; ARABIC SUKUN
+  do (set-unicode-conversion (make-char 'arabic-iso8859-6 iso8859-6)
+                             unicode))
+
+;;;###autoload
+(make-coding-system
+ 'arabic-iso-8bit-with-esc 'iso2022  ;; GNU's iso-8859-6 is
+                                     ;; iso2022-compatible.
+ "ISO-8859-6 (Arabic)"
+ '(charset-g0 ascii
+   charset-g1 arabic-iso8859-6
+   charset-g2 t
+   charset-g3 t
+   no-iso6429 t
+   mnemonic "MIME/Arbc"))
+
--- src/mule-charset.c
+++ src/mule-charset.c
@@ -47,7 +47,6 @@ Lisp_Object Vcharset_latin_iso8859_3;
 Lisp_Object Vcharset_latin_iso8859_4;
 Lisp_Object Vcharset_thai_tis620;
 Lisp_Object Vcharset_greek_iso8859_7;
-Lisp_Object Vcharset_arabic_iso8859_6;
 Lisp_Object Vcharset_hebrew_iso8859_8;
 Lisp_Object Vcharset_katakana_jisx0201;
 Lisp_Object Vcharset_latin_jisx0201;
@@ -94,7 +93,6 @@ Lisp_Object
   Qlatin_iso8859_4,
   Qthai_tis620,
   Qgreek_iso8859_7,
-  Qarabic_iso8859_6,
   Qhebrew_iso8859_8,
   Qkatakana_jisx0201,
   Qlatin_jisx0201,
@@ -1112,7 +1110,6 @@ syms_of_mule_charset (void)
   DEFSYMBOL (Qlatin_iso8859_4);
   DEFSYMBOL (Qthai_tis620);
   DEFSYMBOL (Qgreek_iso8859_7);
-  DEFSYMBOL (Qarabic_iso8859_6);
   DEFSYMBOL (Qhebrew_iso8859_8);
   DEFSYMBOL (Qkatakana_jisx0201);
   DEFSYMBOL (Qlatin_jisx0201);
@@ -1236,15 +1233,6 @@ complex_vars_of_mule_charset (void)
 		  build_msg_string ("ISO8859-7 (Greek)"),
 		  build_msg_string ("ISO8859-7 (Greek)"),
 		  vector1(build_string("iso8859-7")), 0, 0);
-  staticpro (&Vcharset_arabic_iso8859_6);
-  Vcharset_arabic_iso8859_6 =
-    make_charset (LEADING_BYTE_ARABIC_ISO8859_6, Qarabic_iso8859_6, 2,
-		  CHARSET_TYPE_96, 1, 1, 'G',
-		  CHARSET_RIGHT_TO_LEFT,
-		  build_string ("ISO8859-6"),
-		  build_msg_string ("ISO8859-6 (Arabic)"),
-		  build_msg_string ("ISO8859-6 (Arabic)"),
-		  vector1(build_string ("iso8859-6")), 0, 0);
   staticpro (&Vcharset_hebrew_iso8859_8);
   Vcharset_hebrew_iso8859_8 =
     make_charset (LEADING_BYTE_HEBREW_ISO8859_8, Qhebrew_iso8859_8, 2,
