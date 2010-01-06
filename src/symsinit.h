009eeff89d19a04341cfd28984d1280dbe5a2e3f
Jerry James <james@xemacs.org>
2010-01-06 10:01:14 -0700

    Don't create any bignums before pdumping.  Add bignum, ratio, and bigfloat finalizers so we don't leak memory with NEW_GC.  See xemacs-patches message with ID <870180fe1001060900o46fa2b7o26b58850fadf9aba@mail.gmail.com>.

--- src/symsinit.h
+++ src/symsinit.h
@@ -16,8 +16,8 @@ for more details.
 
 You should have received a copy of the GNU General Public License
 along with XEmacs; see the file COPYING.  If not, write to
-the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.  */
+the Free Software Foundation, Inc., 51 Franklin St - Fifth Floor,
+Boston, MA 02111-1301, USA.  */
 
 /* Synched up with: Not in FSF. */
 
@@ -398,6 +398,7 @@ void vars_of_mule_wnn (void);
 void reinit_vars_of_mule_wnn (void);
 void vars_of_nt (void);
 void vars_of_number (void);
+void reinit_vars_of_number (void);
 void vars_of_objects (void);
 void vars_of_font_mgr (void);
 void reinit_vars_of_font_mgr (void);
