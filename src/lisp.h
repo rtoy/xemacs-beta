87ad424b1f912da51b7efd2543b57d806a77ff0a
Ben Wing <ben@xemacs.org>
2010-02-05 08:09:03 -0600

    fix bug #668 (compile error, not --with-debug)
    -------------------- ChangeLog entries follow: --------------------
    
    src/ChangeLog addition:
    
    2010-02-05  Ben Wing  <ben@xemacs.org>
    
    	* lisp.h:
    	* lisp.h (staticpro_nodump_1):
    	Define staticpro_1 and staticpro_nodump_1 when not XEMACS_DEBUG.
    
    	* symbols.c (defsymbol_massage_name_1):
    	* symbols.c (defsymbol_massage_multiword_predicate):
    	Cosmetic fixes.

--- src/lisp.h
+++ src/lisp.h
@@ -4300,10 +4300,20 @@ MODULE_API void unstaticpro_nodump_1 (Lisp_Object *, const Ascbyte *);
 
 /* Call staticpro (&var) to protect static variable `var'. */
 MODULE_API void staticpro (Lisp_Object *);
+/* staticpro_1 (varptr, name) is used when we're not directly calling
+   staticpro() on the address of a Lisp variable, but on a pointer we
+   got from elsewhere.  In that case, NAME is a string describing the
+   actual variable in question.  NAME is used only for debugging purposes,
+   and hence when not DEBUG_XEMACS, staticpro_1() just calls staticpro().
+   With DEBUG_XEMACS, however, it's the reverse -- staticpro() calls
+   staticpro_1(), using the ANSI "stringize" operator to construct a string
+   out of the variable name. */
+#define staticpro_1(ptr, name) staticpro (ptr)
 
 /* Call staticpro_nodump (&var) to protect static variable `var'. */
 /* var will not be saved at dump time */
 MODULE_API void staticpro_nodump (Lisp_Object *);
+#define staticpro_nodump_1(ptr, name) staticpro_nodump (ptr)
 
 #ifdef HAVE_SHLIB
 /* Call unstaticpro_nodump (&var) to stop protecting static variable `var'. */
