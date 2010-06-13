3b192d50c78515e38cc17df6671dae8c6a3a114b
Stephen J. Turnbull <stephen@xemacs.org>
2010-06-13 17:59:42 +0900

    Fix typo in permission notice of elhash.c.

--- src/elhash.c
+++ src/elhash.c
@@ -11,7 +11,7 @@ Free Software Foundation; either version 2, or (at your option) any
 later version.
 
 XEmacs is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCNTABILITY or
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 for more details.
 
