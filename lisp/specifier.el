fcdb2a2605a258400385770e8cb29ef929904d46
Aidan Kehoe <kehoea@parhasard.net>
2010-08-15 15:42:45 +0100

    Be clearer in our error messages, #'canonicalize-inst-pair, #'canonicalize-spec
    lisp/ChangeLog addition:
    
    2010-08-15  Aidan Kehoe  <kehoea@parhasard.net>
    
    	* specifier.el (canonicalize-inst-pair, canonicalize-spec):
    	If a specifier tag set is correct, but an instantiator is not in
    	an accepted format, don't error with the message "Invalid
    	specifier tag set".
    	Also, when we error, use error-symbols, for better structured
    	error handling and more ease when testing.
    
    tests/ChangeLog addition:
    
    2010-08-15  Aidan Kehoe  <kehoea@parhasard.net>
    
    	* automated/lisp-tests.el:
    	(not, not, invalid-argument, invalid-argument):
    	Check that error messages from the image specifier instantiator
    	code are clearer than they used to be.

--- lisp/specifier.el
+++ lisp/specifier.el
@@ -105,20 +105,23 @@ otherwise return t."
 	   ;; this will signal an appropriate error.
 	   (check-valid-instantiator inst-pair specifier-type)))
 
-	((and (valid-specifier-tag-p (car inst-pair))
-	      (valid-instantiator-p (cdr inst-pair) specifier-type))
+	((not (valid-instantiator-p (cdr inst-pair) specifier-type))
+	 (if noerror
+	     t
+	   (check-valid-instantiator (cdr inst-pair) specifier-type)))
+
+	((valid-specifier-tag-p (car inst-pair))
 	 ;; case (b)
 	 (cons (list (car inst-pair)) (cdr inst-pair)))
 
-	((and (valid-specifier-tag-set-p (car inst-pair))
-	      (valid-instantiator-p (cdr inst-pair) specifier-type))
+	((valid-specifier-tag-set-p (car inst-pair))
 	 ;; case (c)
 	 inst-pair)
 	 
 	(t
 	 (if noerror t
-	   (signal 'error (list "Invalid specifier tag set"
-				(car inst-pair)))))))
+	   (error 'invalid-argument "Invalid specifier tag set"
+		  (car inst-pair))))))
 
 (defun canonicalize-inst-list (inst-list specifier-type &optional noerror)
   "Canonicalize the given INST-LIST (a list of inst-pairs).
@@ -199,9 +202,14 @@ otherwise return t."
 
 	(if (not (valid-specifier-locale-p (car spec)))
 	    ;; invalid locale.
-	    (if noerror t
-	      (signal 'error (list "Invalid specifier locale" (car spec))))
-
+	    (if noerror
+		t
+	      (if (consp (car spec))
+		  ;; If it's a cons, they're probably not passing a locale
+		  (error 'invalid-argument
+			 "Not a valid instantiator list" spec)
+		(error 'invalid-argument
+		       "Invalid specifier locale" (car spec))))
 	  ;; case (b)
 	  (let ((result (canonicalize-inst-list (cdr spec) specifier-type
 						noerror)))
