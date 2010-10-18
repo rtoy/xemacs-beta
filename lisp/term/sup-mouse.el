3600273f3ff30f77262c1427fe95ad335cdcecf3
Stephen J. Turnbull <stephen@xemacs.org>
2010-10-18 23:03:27 +0900

    More permission consistency.

--- lisp/term/sup-mouse.el
+++ lisp/term/sup-mouse.el
@@ -1,6 +1,6 @@
 ;;; sup-mouse.el --- supdup mouse support for lisp machines
 
-;; Copyright (C) Free Software Foundation 1985, 1986
+;; Copyright (C) 1985, 1986 Free Software Foundation
 
 ;; Author: Wolfgang Rupprecht
 ;; Maintainer: FSF
@@ -9,20 +9,20 @@
 
 ;;     (from code originally written by John Robinson@bbn for the bitgraph)
 
-;; This file is part of GNU Emacs.
+;; This file is part of XEmacs.
 
-;; GNU Emacs is free software; you can redistribute it and/or modify
+;; XEmacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; XEmacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; along with XEmacs; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ;;; Code:
