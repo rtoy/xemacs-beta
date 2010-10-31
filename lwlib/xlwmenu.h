7f558426d3ec5e5db98540ba2d5a97945ded07bc
Mike Sperber <sperber@deinprogramm.de>
2010-10-31 01:25:03 +0100

    Migrate lwlib/ to GPLv3.

--- lwlib/xlwmenu.h
+++ lwlib/xlwmenu.h
@@ -1,3 +1,23 @@
+/* Implements a lightweight menubar widget.
+   Copyright (C) 1992, 1993, 1994 Lucid, Inc.
+   Copyright (C) 1995 Tinker Systems and INS Engineering Corp.
+
+This file is part of the Lucid Widget Library.
+
+The Lucid Widget Library is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+The Lucid Widget Library is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the Lucid Widget Library.  If not, see
+<http://www.gnu.org/licenses/>. */
+
 #ifndef INCLUDED_xlwmenu_h_
 #define INCLUDED_xlwmenu_h_
 
