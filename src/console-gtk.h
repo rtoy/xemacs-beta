6ee14742366b5d50aa64df01c9773a9e83208770
Jeff Sparkes <jsparkes@gmail.com>
2013-01-20 15:31:38 -0500

    Remove unneeded function prototype.

--- src/console-gtk.h
+++ src/console-gtk.h
@@ -65,9 +65,6 @@ void gtk_output_string (struct window *w, struct display_line *dl,
 		      int start_pixpos, int width, face_index findex,
 		      int cursor, int cursor_start, int cursor_width,
 		      int cursor_height);
-static void gtk_draw_pixbuf (GdkDrawable *drawable, GdkGC *gc,
-                             GdkPixbuf *src, gint xsrc, gint ysrc,
-                             gint xdest, gint ydest, gint width, gint height);
 GdkGC *gtk_get_gc (struct frame *f,
 		   Lisp_Object font, Lisp_Object fg, Lisp_Object bg,
 		   Lisp_Object bg_pmap, Lisp_Object bg_placement,
