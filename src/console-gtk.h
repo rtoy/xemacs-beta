e4ef218924a01cd172766ed13ef72f3b8e2b2f17
Jeff Sparkes <jsparkes@gmail.com>
2013-10-06 14:43:25 -0400

    Don't include gccache-gtk any more.
    	* Makefile.in.in (gtk_objs): Remove gccache-gtk from list.
    
    	* device-gtk.c: Remove include GC cache header.
    	(gtk_init_device): No longer have GC cache.
    	(gtk_delete_device): Ditto.
    
    	* console-xlike-inc.h: Remove include of GC cache header.
    
    	* console-gtk.h: Remove declaration of gtk_get_gc.
    
    	* console-gtk-impl.h (gtk_device): Remove gc-cache from gtk_device
    	structure.

--- src/console-gtk.h
+++ src/console-gtk.h
@@ -65,10 +65,6 @@ void gtk_output_string (struct window *w, struct display_line *dl,
 		      int start_pixpos, int width, face_index findex,
 		      int cursor, int cursor_start, int cursor_width,
 		      int cursor_height);
-GdkGC *gtk_get_gc (struct frame *f,
-		   Lisp_Object font, Lisp_Object fg, Lisp_Object bg,
-		   Lisp_Object bg_pmap, Lisp_Object bg_placement,
-		   Lisp_Object lwidth);
 
 int gtk_initialize_frame_menubar (struct frame *f);
 void gtk_init_modifier_mapping (struct device *d);
